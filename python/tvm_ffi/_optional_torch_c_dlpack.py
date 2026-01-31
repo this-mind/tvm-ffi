09943cf15d60b5a7dd670b03747c87d442ff686a
this.mind <257859507+this-mind@users.noreply.github.com>
2026-01-31 23:45:33 +0100

    hack to disable torch-c-dlpack-ext

--- python/tvm_ffi/_optional_torch_c_dlpack.py
+++ python/tvm_ffi/_optional_torch_c_dlpack.py
@@ -88,8 +88,9 @@ def _check_and_update_dlpack_c_exchange_api(tensor_cls: object) -> bool:
         return True
     return False
 
-
 def load_torch_c_dlpack_extension() -> Any:  # noqa: PLR0912, PLR0915
+	return None
+def skipped_load_torch_c_dlpack_extension() -> Any:  # noqa: PLR0912, PLR0915
     try:
         import torch  # noqa: PLC0415
 
